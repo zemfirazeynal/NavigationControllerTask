cc5f24e89140d27a0db0609142629404cc436f98
Zemfira Zeynalli <109147342+zemfirazeynal@users.noreply.github.com>
2025-03-25 13:09:09 -0700

    navigation controller task

--- NavigationControllerTask/RegisterController.swift
+++ NavigationControllerTask/RegisterController.swift
@@ -7,23 +7,49 @@
 
 import UIKit
 
+//BA 2
+protocol RegisterDelegate {
+    func registerAction(fullname: String, email: String, password: String)
+}
+
 class RegisterController: UIViewController {
 
+    @IBOutlet weak var fullnameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    //AB 1
+    var titleString = ""
+    
+    // BA Delegate 1
+    var delegate: RegisterDelegate?
+    
+    //BA Closure 1
+    var registerCallback: ((String, String, String) -> Void)?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //AB 2
+        title = titleString
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func registerButtonTapped(_ sender: Any) {
+        if let fullname = fullnameTextField.text, !fullname.isEmpty,
+           let email = emailTextField.text, !email.isEmpty,
+           let password = passwordTextField.text, !password.isEmpty {
+            //BA Delegate 3
+//            delegate?.registerAction(fullname: fullname, email: email, password: password)
+            
+            //BA Closure 2
+            registerCallback?(fullname, email, password)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+            navigationController?.popViewController(animated: true)
+        }
+//        dismiss(animated: true)
     }
-    */
+    
+   
 
 }
--- NavigationControllerTask/ViewController.swift
+++ NavigationControllerTask/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+                                   //BA Delegate 5
+class ViewController: UIViewController, RegisterDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        let controller = storyboard?.instantiateViewController(identifier: "RegisterController") as! RegisterController
+        //AB 3
+        controller.titleString = "Qeydiyyat ekranı"
+        //BA Delegate 4
+//        controller.delegate = self
+        
+        //BA Closure 3
+        controller.registerCallback = { fullname, email, password in
+            self.emailTextField.text = email
+            self.passwordTextField.text = password
+            
+        }
+        navigationController?.show(controller, sender: nil)
+//        navigationController?.present(controller, animated: true)
+    }
+    
+    //BA Delegate 6
+    func registerAction(fullname: String, email: String, password: String) {
+        emailTextField.text = email
+        passwordTextField.text = password
+    }
+    
 }
 
