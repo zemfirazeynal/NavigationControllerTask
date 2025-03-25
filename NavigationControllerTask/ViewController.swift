180941b18970dec6f0b291bb1c9c76b125e7c1dc
Zemfira Zeynalli <109147342+zemfirazeynal@users.noreply.github.com>
2025-03-25 07:23:43 -0700

    Initial Commit

---
+++ NavigationControllerTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationControllerTask
+//
+//  Created by Zemfira Asadzade on 25.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
